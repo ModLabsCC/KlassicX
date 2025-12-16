1da5f580b26b5e3ea5aa3df91df649dd535c06e8
Liam Sage <mleaked@gmail.com>
2025-12-16 17:11:22 +0100

    Update logback-classic dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
-    api("ch.qos.logback:logback-classic:1.5.18")
+    api("ch.qos.logback:logback-classic:1.5.22")
     api("io.github.cdimascio:dotenv-kotlin:6.5.1")
     api("dev.fruxz:ascend:2025.7-8af65e5")
     api("com.google.code.gson:gson:2.13.1")
